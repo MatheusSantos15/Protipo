06bbfc4e8f06b073425f5c504468d185f2b8baa8
Matheus Santos <msantosnascimento1504@gmail.com>
2023-11-26 17:27:53 -0300

    cadastro

---
+++ java/scriptcad.js
@@ -0,0 +1,8 @@
+function signup() {
+    alert("Cadastro realizado com sucesso");
+}
+
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+}
--- java/scriptlogin.js
+++ java/scriptlogin.js
@@ -1,4 +1,4 @@
 function login() {
     // Aqui você pode adicionar a lógica de autenticação, por exemplo, enviando uma solicitação para um servidor.
-    alert("Login button clicked");
+    alert("Login feito com sucesso");
 }
